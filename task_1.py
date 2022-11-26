f8a1351724c8a20cc8dfc41e1b8edb9c0198db9b
@Gross-A <shura-35@yandex.ru>
2022-11-26 09:13:26 +0300

    fix name of variables

--- task_1.py
+++ task_1.py
@@ -1,5 +1,6 @@
 # task1
 
+
 # def vocation():
 #     day = int(input('введите день недели от 1 до 7: '))
 #     if day == 6:
@@ -9,36 +10,11 @@
 #     else:
 #         print('нет')
 
+
 # vocation()
 
+
 # task2
-
-# 3
-# task3
-
-# def defineRange():
-#     variant = {
-#         1: 'x > 0, y > 0',
-#         2: 'x < 0, y > 0',
-#         3: 'x < 0, y < 0',
-#         4: 'x > 0, y < 0',
-#     }
-#     # print (variant[1])
-#     x = int(input('введите интересующую четверть: '))
-#     print('точка на оси: ', variant[x])
-
-# defineRange()
-
-# task4 округляет до второго знака после запятой по арифметическим правилам
-# def calcDistance():
-#     pointA = input('input coordinate of pointA x,y:  ').split(',')
-#     pointB = input('input coordinate of pointB x,y:  ').split(',')
-
-#     s = round((abs(int(pointA[0]) - int(pointB[0]))**2 + abs(int(pointA[1]) - int(pointB[1]))**2)**0.5, 2)
-
-#     print('length:  ', s)
-
-# calcDistance()
 
 # task2
 
@@ -61,6 +37,32 @@ if(countR == count):
 
 
 
+# task3
+
+# def define_range():
+#     variant = {
+#         1: 'x > 0, y > 0',
+#         2: 'x < 0, y > 0',
+#         3: 'x < 0, y < 0',
+#         4: 'x > 0, y < 0',
+#     }
+#     x = int(input('введите интересующую четверть: '))
+#     print('точка на оси: ', variant[x])
 
 
+# define_range()
 
+
+# task4 округляет до второго знака после запятой по арифметическим правилам
+
+
+# def calc_distance():
+#     pointA = input('input coordinate of pointA x,y:  ').split(',')
+#     pointB = input('input coordinate of pointB x,y:  ').split(',')
+
+#     s = round((abs(int(pointA[0]) - int(pointB[0]))**2 + abs(int(pointA[1]) - int(pointB[1]))**2)**0.5, 2)
+
+#     print('length:  ', s)
+
+
+# calc_distance()
